456fc4aa685c7f38682a3ca828d589a60d0163bf
Santiago Caci <santiago.caci@gmail.com>
2022-03-27 03:11:10 -0300

    Modificamos componentes.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 import './styles.css';
 
 const todoList = new TodoList();
-const tarea = new Todo('Aprender Javascript');
+const tarea = new Todo('Aprender Javascript!!!');
 
 todoList.insertarTodo(tarea);
 
+
 console.log(todoList);
+
+crearTodoHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,18 +1,21 @@
-import '../css/componentes.css';
-// import webpacklogo from '../assets/img/webpack-logo.png';
+// Referencias en el Html
 
+const divTodoList = document.querySelector('.todo-list');
 
-export const saludar = ( nombre = 'sin nombre' ) => {
-    console.log('Creando etiqueta h1');
+export const crearTodoHtml = (todo) => {
+    const htmlTodo = `
+    <li class="${(todo.completado ? 'completed' : '')}" data-id="${todo.id}">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado ? 'checked' : '')}>
+            <label>${todo.tarea}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${ nombre }`;
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
 
-    document.body.append( h1 );
-
-    
-    // Img
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append( img );
+    divTodoList.appendChild(div.firstElementChild);
+    return div.firstElementChild;
 }
